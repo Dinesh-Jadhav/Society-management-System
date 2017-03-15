1e5e9caa574dead4e509bdbe2e744427af58550d
kalika <sajjanshettyab@gmail.com>
2017-03-15 20:22:41 +0530

    changes

--- db.js
+++ db.js
@@ -1,13 +1,13 @@
 var mySQL = require('mysql');
 var pool = mySQL.createPool({
-    /*host: 'us-cdbr-iron-east-04.cleardb.net',
+    host: 'us-cdbr-iron-east-04.cleardb.net',
     user: 'b1f4a2eb60ab02',
     password: 'f7192840',
     database: 'heroku_e1d0568a4f3b257'
-    */host:  'localhost',
+    /*host:  'localhost',
 	user:'root',
 	password:'',
-	database:'society_management'
+	database:'society_management'*/
 });
 
 module.exports = pool;
